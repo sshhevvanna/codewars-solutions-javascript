1c21ce7604c375e162f68e9e2724cd533626195c
sh9vch9nk0 <sh9vch9nk0@gmail.com>
2023-02-01 18:26:28 +0200

    Added 2 solutions

---
+++ js/6kyu/Highest Scoring Word.js
@@ -0,0 +1,61 @@
+/*Given a string of words, you need to find the highest scoring word.
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+For example, the score of abad is 8 (1 + 2 + 1 + 4).
+You need to return the highest scoring word as a string.
+If two words score the same, return the word that appears earliest in the original string.
+All letters will be lowercase and all inputs will be valid.*/
+
+function high(x) {
+  let array = x.split(" ");
+  let newArr = [];
+  let biggestNum = [];
+  let answer = 0;
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  array.forEach((word) => {
+    let sum = 0;
+    for (let i = 0; i <= word.length - 1; i += 1) {
+      sum += alphabet.indexOf(word[i]) + 1;
+    }
+    newArr.push(sum);
+    biggestNum.push(sum);
+  });
+
+  newArr.sort(function (a, b) {
+    return b - a;
+  });
+
+  for (let i = 0; i <= biggestNum.length - 1; i += 1) {
+    if (newArr[0] === biggestNum[i]) {
+      answer = biggestNum.indexOf(biggestNum[i]);
+    }
+  }
+  return array[answer];
+}
+console.log(high("man i need a taxi up to ubud"));
---
+++ js/7kyu/Two to One.js
@@ -0,0 +1,36 @@
+/*Take 2 strings s1 and s2 including only letters from a to z. 
+Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"*/
+
+//Solution #1
+function longest(s1, s2) {
+  s1 = s1.split("");
+  s2 = s2.split("");
+  s1.push(...s2);
+  let array = s1.sort();
+  let newArr = [];
+  for (let i = 0; i <= array.length - 1; i += 1) {
+    if (array[i] != array[i + 1]) {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr.join("");
+}
+console.log(longest("aretheyhere", "yestheyarehere"));
+
+//Solution #2
+const longest = (s1, s2) => [...new Set(s1 + s2)].sort().join("");
+
+//Solution #3
+function longest(s1, s2) {
+  return Array.from(new Set(s1 + s2))
+    .sort()
+    .join("");
+}
