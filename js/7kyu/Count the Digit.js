99f20512fabf047c5c31fe474f89726a672ef50d
sh9vch9nk0 <sh9vch9nk0@gmail.com>
2023-02-09 21:42:41 +0200

    Added 10 solutions

---
+++ js/7kyu/Count the Digit.js
@@ -0,0 +1,39 @@
+/*Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+Square all numbers k (0 <= k <= n) between 0 and n.
+
+Count the numbers of digits d used in the writing of all the k**2.
+
+Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+
+Examples:
+n = 10, d = 1 
+the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+nb_dig(25, 1) returns 11 since
+the k*k that contain the digit 1 are:
+1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+So there are 11 digits 1 for the squares of numbers between 0 and 25.
+Note that 121 has twice the digit 1.*/
+
+function nbDig(n, d) {
+  let array = [];
+  let string = "";
+  let total = 0;
+
+  for (let i = 0; i <= n; i += 1) {
+    array.push(i);
+  }
+
+  array = array.map((num) => num ** 2);
+  string = array.join("");
+
+  for (let i = 0; i <= string.length - 1; i += 1) {
+    if (string[i] == d) {
+      total += 1;
+    }
+  }
+  return total;
+}
+console.log(nbDig(10, 1));
---
+++ js/7kyu/Vowel Count.js
@@ -0,0 +1,14 @@
+/*Return the number (count) of vowels in the given string.
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+The input string will only consist of lower case letters and/or spaces.*/
+
+function getCount(str) {
+  let total = 0;
+  let regexp = /[aeiou]/gi;
+  for (let i = 0; i <= str.length - 1; i += 1) {
+    if (str[i].match(regexp)) {
+      total += 1;
+    }
+  }
+  return total;
+}
